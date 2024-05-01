b320ceff27178f783ff0d4f611eeccc31ec10384
Mauricio Sebastian Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-05-01 07:51:09 -0500

    feat(home-administrator): add cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
 import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatCardSubtitle} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -29,7 +31,13 @@ import {HttpClientModule} from "@angular/common/http";
     MatCheckboxModule,
     MatFormFieldModule,
     MatIcon,
-    HttpClientModule
+    HttpClientModule,
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
