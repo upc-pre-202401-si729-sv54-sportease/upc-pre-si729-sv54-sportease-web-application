d5941e0c3c3f7cfca872b2d63036d33c8f6217a4
Flavio Trigueros <U202210190@upc.edu.pe>
2024-05-28 19:23:23 -0500

    feat: add payment-management section

---
+++ src/app/sportease/pages/payment-management-students/payment-management-students.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentService } from '../../services/payment.service.spect.ts';
+
+@Component({
+  selector: 'app-payment-management-students',
+  templateUrl: './payment-management-students.component.html',
+  styleUrls: ['./payment-management-students.component.css']
+})
+export class PaymentManagementStudentsComponent implements OnInit {
+  transactions: any[] = [];
+  filteredTransactions: any[] = [];
+  selectedStudentId: string = '1';
+
+  constructor(private paymentService: PaymentService) { }
+
+  ngOnInit(): void {
+    this.paymentService.getTransactions().subscribe(data => {
+      this.transactions = data;
+      this.filterTransactions();
+    });
+  }
+
+  filterTransactions(): void {
+    this.filteredTransactions = this.transactions.filter(transaction => transaction['id-student'] === this.selectedStudentId);
+  }
+}
---
+++ src/app/sportease/pages/payment-management/payment-management.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {PaymentService} from "../../services/payment.service.spect.ts";
+
+@Component({
+  selector: 'app-payment-management-component',
+  templateUrl: './payment-management.component.html',
+  styleUrls: ['./payment-management.component.css']
+})
+export class PaymentManagementComponent implements OnInit {
+  transactions!: any[];
+
+  constructor(private paymentService: PaymentService) { }
+
+  ngOnInit(): void {
+    this.paymentService.getTransactions().subscribe(data => {
+      this.transactions = data;
+    });
+  }
+}
