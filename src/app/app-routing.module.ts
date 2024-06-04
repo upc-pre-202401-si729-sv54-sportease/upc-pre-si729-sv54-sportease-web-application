48ffa00de582842ef16d1b7472e5464c4c6b54f3
FlavioTrigueros <130616378+FlavioTrigueros@users.noreply.github.com>
2024-06-04 00:57:57 -0500

    feat: Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PaymentManagementComponent} from "./sportease/pages/payment-management/payment-management.component";
+import {
+  PaymentManagementStudentsComponent
+} from "./sportease/pages/payment-management-students/payment-management-students.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'payments/management', component: PaymentManagementComponent },
+  { path: 'payments/students', component: PaymentManagementStudentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
