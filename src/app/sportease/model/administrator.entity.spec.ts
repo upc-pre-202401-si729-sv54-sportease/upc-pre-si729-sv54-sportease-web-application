5df1fb6c34e6b09cfd90dc07413e098d65d0b65c
FlavioTrigueros <130616378+FlavioTrigueros@users.noreply.github.com>
2024-06-04 00:49:22 -0500

    feat: administrator.entity.spect.ts

---
+++ src/app/sportease/model/administrator.entity.spec.ts
@@ -0,0 +1,7 @@
+import { Administrator } from './administrator.entity';
+
+describe('Administrator', () => {
+  it('should create an instance', () => {
+    expect(new Administrator()).toBeTruthy();
+  });
+});
